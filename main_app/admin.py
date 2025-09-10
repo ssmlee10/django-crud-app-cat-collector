1cd6121183eb1b89f985d01194a53500a964abd1
Sharon Lee <ssmlee10@gmail.com>
2025-09-10 09:33:16 -0700

    add feeding model, works on front end

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Cat
+from .models import Cat, Feeding
 
 # Register your models here.
 # this brings in the cat model into the admin site
 admin.site.register(Cat)
+admin.site.register(Feeding)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+  # first is the lette that goes into database, second is the user friendly value for meal
+  # represents the drop down selection
+)
+
 class Cat(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -12,3 +20,20 @@ class Cat(models.Model):
   
   def get_absolute_url(self):
     return reverse('cat-detail', kwargs={ 'cat_id': self.id })
+
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices = MEALS,
+    default = MEALS[0][0]
+  )
+  cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+  # first argument is Cat model
+  # meal has to be created and has to be associated with this specific cat
+  # if the cat happens to be deleted, the associated meals will also be deleted (cascade)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+  # method that automatically gets the human friendly value of breakfast, lunch, dinner
+  #override dunder string method
